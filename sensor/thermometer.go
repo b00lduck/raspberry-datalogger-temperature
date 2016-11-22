3f5083831e783af9a72f04eeb0e3b879bc25ad9e
Daniel Zerlett <daniel@zerlett.eu>
2016-11-22 19:34:24 +0100

    added 1-wire thermo support

--- sensor/thermometer.go
+++ sensor/thermometer.go
@@ -2,37 +2,64 @@ package sensor
 import (
 	"math"
 	"github.com/b00lduck/raspberry-datalogger-dataservice-client"
-	"fmt"
+	"io/ioutil"
+	log "github.com/Sirupsen/logrus"
+	"strings"
+	"strconv"
 )
 
 type Thermometer struct {
 	oldValue float64
 	precision float64
 	code string
+	filename string
 }
 
-func NewThermometer(code string, precision float64) Thermometer {
+const thermoDevicePrefix = "/sys/devices/w1_bus_master1/"
+const thermoDeviceSuffix = "/w1_slave"
+
+func NewThermometer(code string, filename string, precision float64) Thermometer {
 	return Thermometer{
 		oldValue: 0,
 		precision: precision,
 		code: code,
+		filename: thermoDevicePrefix + filename + thermoDeviceSuffix,
 	}
 }
 
-func (t *Thermometer) SetNewReading(reading float64) {
+func (t *Thermometer) Process() {
+
+	rawData, err := ioutil.ReadFile(t.filename)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	tempInt, err := strconv.Atoi(strings.Split(string(rawData), "t=")[1])
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	temp := float64(tempInt) / 1000
+
+	t.setNewReading(temp)
+}
+
+func (t *Thermometer) setNewReading(reading float64) {
 
 	// precision reduction
-	limitedPrecisionValue := Round(reading / t.precision) * t.precision
+	limitedPrecisionValue := round(reading / t.precision) * t.precision
 
 	if math.Abs(float64(limitedPrecisionValue - t.oldValue)) > t.precision {
 		if err := client.SendThermometerReading(t.code, limitedPrecisionValue); err != nil {
-			fmt.Println(err)
+			log.Error(err)
 		} else {
 			t.oldValue = limitedPrecisionValue
 		}
 	}
 }
 
-func Round(x float64) float64 {
+func round(x float64) float64 {
 	return math.Floor(x + 0.5)
 }
--- temperature.go
+++ temperature.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"time"
 	log "github.com/Sirupsen/logrus"
-        "sensor"
+	"github.com/b00lduck/raspberry-datalogger-temperature/sensor"
 )
 
 func main() {
     log.Info("Starting temperature acqusition service")
     defer log.Info("Exiting temperature acqusition service")
+
+    var therm1 = sensor.NewThermometer("THERMO_1", "28-031655cbb5ff", 0.1)
+    var therm2 = sensor.NewThermometer("THERMO_2", "28-041658b369ff", 0.1)
+    var therm3 = sensor.NewThermometer("THERMO_3", "28-041658b3cbff", 0.1)
+
     for {
+	therm1.Process()
+	therm2.Process()
+	therm3.Process()
 	time.Sleep(5 * time.Second)
     }
 }
+
 
