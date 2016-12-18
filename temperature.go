f84c11dc177ea6948de851bfa804ff5b8da44b89
Daniel Zerlett <daniel@zerlett.eu>
2016-12-18 23:19:49 +0000

    changed IDs

--- temperature.go
+++ temperature.go
@@ -10,9 +10,9 @@ func main() {
     log.Info("Starting temperature acqusition service")
     defer log.Info("Exiting temperature acqusition service")
 
-    var therm1 = sensor.NewThermometer("THERMO_1", "28-031655cbb5ff", 0.1)
-    var therm2 = sensor.NewThermometer("THERMO_2", "28-041658b369ff", 0.1)
-    var therm3 = sensor.NewThermometer("THERMO_3", "28-041658b3cbff", 0.1)
+    var therm1 = sensor.NewThermometer("temperature_heating_room", "28-031655cbb5ff", 0.1)
+    var therm2 = sensor.NewThermometer("temperature_test_2", "28-041658b369ff", 0.1)
+    var therm3 = sensor.NewThermometer("temperature_test_3", "28-041658b3cbff", 0.1)
 
     for {
 	therm1.Process()
